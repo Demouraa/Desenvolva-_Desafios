1d793151ce18fa617cd29f9302c4e40e12cc6648
Matheus DeMoura <matheusdemoura@MacBook-Pro-de-Matheus.local>
2025-07-30 21:34:58 -0300

    primeiro commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        Produto p1 = new Produto("Camisa", 59.90, 10);
+        Produto p2 = new Produto("Calça", 89.90);
+
+        p2.adicionarEstoque(5);
+        p1.vender(3);
+        p2.vender(10);
+
+        p1.exibirResumo();
+        p2.exibirResumo();
+    }
+}
---
+++ src/Produto.java
@@ -0,0 +1,47 @@
+public class Produto {
+    private String nome;
+    private double preco;
+    private int estoque;
+
+    // Dois construtores
+    // 1 Construtor que define todos os atributos
+    public Produto(String nome, double preco, int estoque) {
+        this.nome = nome;
+        this.preco = preco;
+        this.estoque = estoque;
+    }
+
+    //1 Construtor que define apenas nome e preco, com estoque = 0
+    public Produto(String nome, double preco) {
+        this.nome = nome;
+        this.preco = preco;
+        this.estoque = 0;
+    }
+
+    //Metodo para adicionar ao estoque
+    public void adicionarEstoque(int quantidade) {
+        if (quantidade > 0) {
+            estoque += quantidade;
+        }
+    }
+
+    //Metodo para vender produtos (só se houver estoque suficiente)
+    public void vender(int quantidade) {
+        if (quantidade <= 0) {
+            System.out.println("Quantidade inválida para venda.");
+        } else if (quantidade > 0 && quantidade <= estoque) {
+            this.estoque -= quantidade;
+            System.out.println("Venda realizada com sucesso.");
+        } else {
+            System.out.println("Estoque insuficiente para a venda.");
+        }
+    }
+
+    //Metodo para exibir resumo do produto
+    public void exibirResumo() {
+        System.out.println("Produto: " + nome);
+        System.out.println("Preço: R$ " + preco);
+        System.out.println("Estoque: " + estoque + " unidades");
+    }
+}
+
